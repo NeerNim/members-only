491947c6eabd385e6f7b1831cb027a3664b00896
NeerNim <neernimt@gmail.com>
2019-07-09 17:03:32 +0200

    add new column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
 	has_secure_password
 
 	validates :name, :email, presence: true
+	
+	before_create :create_user
+		
+	private 
+
+	def create_user
+		remember_token = SecureRandom.urlsafe_base64.to_s
+		User.create({remember_token: Digest::SHA1.hexdigest("remember_token")})	
+	end
+
+
 end
