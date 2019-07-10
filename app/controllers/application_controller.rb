50f0332265863981cd55001958896b310ff4227d
whiz25 <afanimartin@gmail.com>
2019-07-10 16:37:38 +0300

    Add sign out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  def sign_out
+    cookies.delete(:remember_token)
+    cookies.delete(:user_id)
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,7 @@ module SessionsHelper
   def log_in(user)
     cookies.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+    current_user
   end
   def current_user
     @current_user ||= User.find_by(remember_token: cookies[:remember_token])  
