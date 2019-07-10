ac93fc4c44e382453a611c046e84780d195504ef
NeerNim <neernimt@gmail.com>
2019-07-10 15:24:55 +0200

    add cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,13 @@
 module SessionsHelper
   def log_in(user)
-    session[:user_id] = user.id
+    cookies.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
   def current_user
-      if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end  
+    @current_user ||= User.find_by(remember_token: cookies[:remember_token])  
   end
   def logged_in?
     !current_user.nil?
   end
+  
 end
