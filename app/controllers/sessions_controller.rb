e40f029652462336283a96e7b21408763b97958d
whiz25 <afanimartin@gmail.com>
2019-07-10 13:08:17 +0300

    Authenticate user before log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
-    session[:current_user_id] = @user.id
   end
   
   def create  
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy    
