c4eadbdb478415e126a456f26efdc027a99b8835
NeerNim <neernimt@gmail.com>
2019-07-11 12:14:03 +0200

    add posts to the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :posts
   resources :posts, only: [:new, :create, :index] 
-  root to: 'users#new'
+  root to: 'posts#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
   
   resources :users
   resources :sessions
+  
 end
