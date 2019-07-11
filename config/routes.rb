b5cbea638b90901c0220da526500a99e187f4940
NeerNim <neernimt@gmail.com>
2019-07-11 11:45:26 +0200

    Fix routes for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
   
   resources :users
+  resources :sessions
 end
